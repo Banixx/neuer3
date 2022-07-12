d50793f44f01c460e14d83e0b6629e132e436914
banixx <banidoesch@gmail.com>
2022-07-12 19:50:27 +0200

    kommitet vo mir

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-let go =  function(){
-hoi ="sali";
-let ele = document.getElementById("hoi");
-ele.innerHTML="Hellou";
+let go = function () {
+  hoi = "sali";
+  let ele = document.getElementById("hoi");
+  ele.innerHTML = "Hellou";
+};
 
+function goit() {
+  document.write("TEST");
 }
-(function () { 
-document.write("TEST"
- })();
+console.log("hoi");
+goit();
